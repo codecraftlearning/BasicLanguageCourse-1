ea045bb0943b30826ba982e89aeacb489eb41dc5
codecraftlearning <codecraftlearning@gmail.com>
2025-09-25 20:00:32 +0530

    main function session

--- main.cpp
+++ main.cpp
@@ -1,1 +1,12 @@
-//some code here
+#include <iostream>
+
+int main(int numberOfArgs, char* argsValues[]) {
+    if (numberOfArgs > 1) {
+        int sum = 0;
+        for (int i = 1; i<numberOfArgs; i++) {
+            sum += std::atoi(argsValues[i]);
+        }
+
+        std::cout << sum;
+    }
+}

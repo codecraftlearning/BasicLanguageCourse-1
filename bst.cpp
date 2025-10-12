388ea137bce98262679e9a911ddacbbec551f3e2
codecraftlearning <codecraftlearning@gmail.com>
2025-10-12 16:13:41 +0530

    tree and bst

---
+++ bst.cpp
@@ -0,0 +1,160 @@
+#include <iostream>
+
+using namespace std;
+
+struct Node
+{
+    int data;
+    Node *left;
+    Node *right;
+
+    Node(int val)
+    {
+        this->data = val;
+        this->left = nullptr;
+        this->right = nullptr;
+    }
+};
+
+Node *insert(Node *root, int value)
+{
+    if (root == nullptr)
+    {
+        return new Node(value);
+    }
+
+    if (value < root->data)
+    {
+        root->left = insert(root->left, value);
+    }
+    else
+    {
+        root->right = insert(root->right, value);
+    }
+
+    return root;
+}
+
+void inorder(Node *root)
+{
+    if (root == nullptr)
+    {
+        return;
+    }
+
+    inorder(root->left);
+    cout << root->data << " ";
+    inorder(root->right);
+}
+
+void preorder(Node *root)
+{
+    if (root == nullptr)
+    {
+        return;
+    }
+
+    cout << root->data << " ";
+    preorder(root->left);
+    preorder(root->right);
+}
+
+bool searchBST(Node *root, int value)
+{
+    if (root == nullptr)
+    {
+        return false;
+    }
+
+    if (root->data == value)
+    {
+        return true;
+    }
+
+    if (value < root->data)
+    {
+        return searchBST(root->left, value);
+    }
+    else
+    {
+        return searchBST(root->right, value);
+    }
+}
+
+Node* findMin(Node* root) {
+    while(root && root->left != nullptr) {
+        root = root->left;
+    }
+
+    return root;
+}
+
+Node* deleteNode(Node *root, int value)
+{
+    if (root == nullptr) {
+        return root;
+    }
+
+    if (value < root->data) {
+        root->left = deleteNode(root->left, value);
+    } else if (value > root->data) {
+        root->right = deleteNode(root->right, value);
+    } else {
+
+        if (root->left == nullptr && root->right == nullptr) {
+            delete root;
+            return nullptr;
+        } else if (root->left == nullptr) {
+            Node* temp = root->right;
+            delete root;
+            return temp;
+        } else if (root->right == nullptr) {
+            Node* temp = root->left;
+            delete root;
+            return temp;
+        }
+
+        Node* temp = findMin(root->right);
+        root->data = temp->data;
+        root->right = deleteNode(root->right, temp->data);
+
+    }
+
+    return root;
+}
+
+int main(int size, char *args[])
+{
+    Node *root = nullptr;
+    int arr[] = {50, 30, 20, 40, 70, 60, 80, 75};
+    int n = sizeof(arr) / sizeof(arr[0]);
+
+    for (int i = 0; i < n; i++)
+    {
+        root = insert(root, arr[i]);
+    }
+
+    cout << "Inorder traversal of the BST: ";
+    inorder(root);
+
+    cout << "\nPreorder traversal of the BST: ";
+    preorder(root);
+    cout << endl;
+
+    if (searchBST(root, atoi(args[1])))
+    {
+        cout << "Number available in BST";
+    }
+    else
+    {
+        cout << "Not Found!!";
+    }
+
+    root = deleteNode(root, atoi(args[1]));
+
+    cout << "\nPreorder traversal of the BST after deletion: ";
+    preorder(root);
+    cout << endl;
+
+    return 0;
+}
---
+++ tree.cpp
@@ -0,0 +1,115 @@
+#include <iostream>
+
+using namespace std;
+
+struct Node {
+    int data;
+    Node* left;
+    Node* right;
+
+    Node(int val) {
+        this->data = val;
+        this->left = nullptr;
+        this->right = nullptr;
+    }
+};
+
+Node* buildTree(int preorder[], int& index, int n);
+void inorder(Node* root, bool isRepresentation);
+void preorder(Node* root, bool isRepresentation);
+void postorder(Node* root, bool isRepresentation);
+
+
+int main() {
+
+    // Node* root = new Node(1);
+    // root->left = new Node(2);
+    // root->right = new Node(3);
+    // root->left->left = new Node(4);
+    // root->left->right = new Node(5);
+    // root->right->left = new Node(6);
+    // root->right->right = new Node(7);
+
+    int arr[] = {1, 2, 4, -1, -1, 5, -1, -1, 3, 6, -1, -1, 7, -1, -1};
+    int n = sizeof(arr) / sizeof(arr[0]);
+
+    int index = 0;
+    Node* root = buildTree(arr, index, n);
+
+
+    inorder(root, false);
+    cout << "\n";
+    inorder(root, true);
+    cout << "\n";
+    preorder(root, false);
+    cout << "\n";
+    preorder(root, true);
+    cout << "\n";
+    postorder(root, false);
+    cout << "\n";
+    postorder(root, true);
+    return 0;
+}
+
+Node* buildTree(int preorder[], int& index, int n) {
+    if (index >= n || preorder[index] == -1) {
+        index++;
+        return nullptr;
+    }
+
+    Node* root = new Node(preorder[index++]);
+    root->left = buildTree(preorder, index, n);
+    root->right = buildTree(preorder, index, n);
+
+    return root;
+}
+
+
+void inorder(Node* root, bool isRepresentation) {
+    if (root == nullptr) {
+        if (isRepresentation) {
+            cout << -1 << " ";
+        }
+        return;
+    }
+
+    inorder(root->left, isRepresentation);
+    cout << root->data << " ";
+    inorder(root->right, isRepresentation);
+}
+
+void preorder(Node* root, bool isRepresentation) {
+    if (root == nullptr) {
+        if (isRepresentation) {
+            cout << -1 << " ";
+        }
+        return;
+    }
+
+    cout << root->data << " ";
+    preorder(root->left, isRepresentation);
+    preorder(root->right, isRepresentation);
+}
+
+void postorder(Node* root, bool isRepresentation) {
+    if (root == nullptr) {
+        if (isRepresentation) {
+            cout << -1 << " ";
+        }
+        return;
+    }
+
+    postorder(root->left, isRepresentation);
+    postorder(root->right, isRepresentation);
+    cout << root->data << " ";
+}
+
+
+
+/*
+                    1
+                2       3
+            4     5   6    7
+        -1
+
+*/
